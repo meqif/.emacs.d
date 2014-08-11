ebb00a1e42398db949041cbb3da90453c8dd5d21
Ricardo Martins <ricardo@scarybox.net>
2014-08-11 13:05:06 +0100

    Better version of Sublime Text-like comment shortcut.

--- init.el
+++ init.el
@@ -195,21 +195,15 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 
 ;; Comment or uncomment current line or region
-(defun comment-eclipse ()
-  (interactive)
-  (let ((start (line-beginning-position))
-        (end (line-end-position)))
-    (when (region-active-p)
-      (setq start (save-excursion
-                    (goto-char (region-beginning))
-                    (beginning-of-line)
-                    (point))
-            end (save-excursion
-                  (goto-char (region-end))
-                  (end-of-line)
-                  (point))))
-    (comment-or-uncomment-region start end)))
-(global-set-key (kbd "s-/") 'comment-eclipse)
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+(global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
 
 ;; Use only own snippets, do not use bundled ones
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
