8f347cf45eeddb894f14eb103cbf85d7448ebe79
Ricardo Martins <ricardo@scarybox.net>
2014-09-28 20:21:03 +0100

    Remove redundant and unused config.

--- init.el
+++ init.el
@@ -124,26 +124,6 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'slime-repl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-;; (let ((depth 0))
-;;   (dolist (color
-;;            ;; These are nicked from VIMClojure:
-;;            ;; vimclojure/vim/syntax/clojure.vim
-;;            '("orange1"
-;;              "yellow1"
-;;              "greenyellow"
-;;              "green1"
-;;              "springgreen1"
-;;              "cyan1"
-;;              "slateblue1"
-;;              "magenta1"
-;;              "purple1"))
-;;     (set-face-foreground (intern (format "rainbow-delimiters-depth-%d-face"
-;;                                          (incf depth)))
-;;                          color)))
 
 ;; Enable rainbow delimiters globally
 (global-rainbow-delimiters-mode)
