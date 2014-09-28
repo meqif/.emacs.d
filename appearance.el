c6c9c1aaa3f0e5a286a057947f71fdd672ede7c0
Ricardo Martins <ricardo@scarybox.net>
2014-09-28 19:08:55 +0100

    Short js2-mode's modeline name.

--- appearance.el
+++ appearance.el
@@ -63,4 +63,11 @@
 (eval-after-load "smartparens" '(diminish 'smartparens-mode))
 (eval-after-load "magit" '(diminish 'magit-auto-revert-mode))
 
+(defmacro rename-modeline (package-name mode new-name)
+  `(eval-after-load ,package-name
+     '(defadvice ,mode (after rename-modeline activate)
+        (setq mode-name ,new-name))))
+
+(rename-modeline "js2-mode" js2-mode "JS2")
+
 (provide 'appearance)
