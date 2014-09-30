877335a7794fc40d43b3a4a1fc5545b7ab7418be
Ricardo Martins <ricardo@scarybox.net>
2014-10-01 00:21:45 +0100

    Pull stuff to sane defaults.
    Who am I to decide what is sane, though?

--- init.el
+++ init.el
@@ -13,41 +13,8 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-;; Show keystrokes in progress
-(setq echo-keystrokes 0.1)
-
-;; Tabs
-(setq-default tab-width 4)
-
-;; Disable backup
-(setq backup-inhibited t)
-
-;; Disable auto save
-(setq auto-save-default nil)
-
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
-
-;; Answering just 'y' or 'n' will do
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; UTF-8 please
-(setq locale-coding-system 'utf-8) ; pretty
-(set-terminal-coding-system 'utf-8) ; pretty
-(set-keyboard-coding-system 'utf-8) ; pretty
-(set-selection-coding-system 'utf-8) ; please
-(prefer-coding-system 'utf-8) ; with sugar on top
-
-;; Always display line and column numbers
-(setq line-number-mode t)
-(setq column-number-mode t)
-
-;; Lines should be 80 characters wide, not 72
-;; Make fill-paragraph (Alt-Q and gq) respect the line width
-(set-default 'fill-column 80)
-
-;; Never insert tabs
-(set-default 'indent-tabs-mode nil)
+;; Bring some sanity in
+(require 'sane-defaults)
 
 ;; Default directory
 (setq default-directory "~/")
