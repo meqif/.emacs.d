c2b3c6a691818d985896cb02faf660caeb9c7879
Ricardo Martins <ricardo@scarybox.net>
2014-10-01 12:45:44 +0100

    Add function to rename buffer and file.

--- setup-utils.el
+++ setup-utils.el
@@ -22,4 +22,23 @@
         (car parts)
         (mapconcat 'upcase-initials parts ""))))
 
+;;----------------------------------------------------------------------------
+;; Rename the current file
+;; Source: https://github.com/purcell/emacs.d/blob/master/lisp/init-utils.el
+;;----------------------------------------------------------------------------
+(defun rename-this-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting a file!" name))
+    (if (get-buffer new-name)
+        (message "A buffer named '%s' already exists!" new-name)
+      (progn
+        (when (file-exists-p filename)
+          (rename-file filename new-name 1))
+        (rename-buffer new-name)
+        (set-visited-file-name new-name)))))
+
 (provide 'setup-utils)
