81533d787077e7ddeae2cb6187baa2d2a6af8471
Ricardo Martins <ricardo@scarybox.net>
2014-10-01 12:50:56 +0100

    Remove unused stuff from magnar's configuration.

--- keybindings.el
+++ keybindings.el
@@ -1,17 +1,6 @@
 ;; Window switching
 (windmove-default-keybindings) ;; Shift+direction
 
-;; change command to meta, and ignore option to use weird Norwegian keyboard
-;(setq mac-option-modifier 'super)
-;(setq mac-command-modifier 'meta)
-;(setq ns-function-modifier 'hyper)
-
-;; Norwegian mac-keyboard alt-keys)
-;(define-key key-translation-map (kbd "s-8") (kbd "["))
-;(define-key key-translation-map (kbd "s-(") (kbd "{"))
-;(define-key key-translation-map (kbd "s-9") (kbd "]"))
-;(define-key key-translation-map (kbd "s-)") (kbd "}"))
-
 (setq ns-function-modifier 'hyper)
 
 ; Let right alt work as usual
