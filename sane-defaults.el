6905d5e9282baf57b65f9fe7980203e209666ada
Ricardo Martins <ricardo@scarybox.net>
2014-10-02 10:23:39 +0100

    Increase GC threshold.

--- sane-defaults.el
+++ sane-defaults.el
@@ -44,4 +44,7 @@
 (savehist-mode 1)
 (setq history-length 1000)
 
+;; Don't be so stingy on the memory, we have lots now. It's the distant future.
+(setq gc-cons-threshold 20000000)
+
 (provide 'sane-defaults)
