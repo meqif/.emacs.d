015d3aa3b8850c7c43bee07d7c47f7e5af7b7ef6
Ricardo Martins <ricardo@scarybox.net>
2014-10-16 15:38:26 +0100

    Add better headings in Markdown files.

--- setup-markdown.el
+++ setup-markdown.el
@@ -34,4 +34,22 @@
     (define-key markdown-mode-map (kbd "C-c C-c") 'markdown-toggle-checkbox)
 ))
 
+;; Better headings
+;; (set-face-attribute 'markdown-header-face-1 nil :inherit markdown-header-face :height 2.0)
+;; (set-face-attribute 'markdown-header-face-2 nil :inherit markdown-header-face :height 1.5)
+;; (set-face-attribute 'markdown-header-face-3 nil :inherit markdown-header-face :height 1.25)
+
+(mapcar '(lambda (entry)
+           (let ((face-name (car entry))
+                 (face-attr (cadr entry))
+                 (face-attr-value (car (cddr entry))))
+             (set-face-attribute face-name nil
+                                 :inherit markdown-header-face
+                                 face-attr face-attr-value)))
+        '((markdown-header-face-1 :height 2.2)
+          (markdown-header-face-2 :height 1.7)
+          (markdown-header-face-3 :height 1.5)
+          (markdown-header-face-4 :height 1.3)
+          (markdown-header-face-5 :height 1.1)))
+
 (provide 'setup-markdown)
