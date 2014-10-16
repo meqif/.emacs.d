eb7990936b5e4f162d2c0325cf9f721fef6542ef
Ricardo Martins <ricardo@scarybox.net>
2014-10-16 19:19:59 +0100

    Refactor lambda into function.

--- setup-markdown.el
+++ setup-markdown.el
@@ -37,21 +37,24 @@
 ))
 
 ;; Better headings
+(defun apply-face-attributes (entry)
+  "Apply attribute-value pairs to the face in the head of the argument"
+  (let ((face-name (car entry)))
+    ;; Apply each pair of attribute-value to the current heading
+    (-each (-partition 2 (cdr entry))
+      (lambda (pair)
+        (let ((face-attr (car pair))
+              (face-attr-value (cadr pair)))
+          (set-face-attribute face-name nil
+                              :inherit markdown-header-face
+                              face-attr face-attr-value))))))
+
 (-each
     '((markdown-header-face-1 :height 2.2)
       (markdown-header-face-2 :height 1.7)
       (markdown-header-face-3 :height 1.5)
       (markdown-header-face-4 :height 1.3)
       (markdown-header-face-5 :height 1.1))
-  (lambda (entry)
-     (let ((face-name (car entry)))
-       ;; Apply each pair of attribute-value to the current heading
-       (-each (-partition 2 (cdr entry))
-         (lambda (pair)
-           (let ((face-attr (car pair))
-                 (face-attr-value (cadr pair)))
-             (set-face-attribute face-name nil
-                                 :inherit markdown-header-face
-                                 face-attr face-attr-value)))))))
+  'apply-face-attributes)
 
 (provide 'setup-markdown)
