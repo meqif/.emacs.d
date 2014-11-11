e95389389da829fc6dbc5885937082dda62f0315
Ricardo Martins <ricardo@scarybox.net>
2014-11-11 11:44:26 +0000

    Customize helm-bibtex actions.

--- lisp/setup-helm-bibtex.el
+++ lisp/setup-helm-bibtex.el
@@ -8,4 +8,20 @@
              '(org-mode . (lambda (keys)
                             (format "\\autocite{%s}" (s-join ", " keys)))))
 
+(setq helm-source-bibtex
+      '((name . "BibTeX entries")
+        (init . helm-bibtex-init)
+        (candidates . helm-bibtex-candidates)
+        (filtered-candidate-transformer . helm-bibtex-candidates-formatter)
+        (action
+         ("Insert citation" . helm-bibtex-insert-citation)
+         ("Insert reference" . helm-bibtex-insert-reference)
+         ("Insert BibTeX key" . helm-bibtex-insert-key)
+         ("Insert BibTeX entry" . helm-bibtex-insert-bibtex)
+         ("Attach PDF to email" . helm-bibtex-add-PDF-attachment)
+         ("Edit notes" . helm-bibtex-edit-notes)
+         ("Open PDF file (if present)" . helm-bibtex-open-pdf)
+         ("Open URL or DOI in browser" . helm-bibtex-open-url-or-doi)
+         ("Show entry" . helm-bibtex-show-entry))))
+
 (provide 'setup-helm-bibtex)
