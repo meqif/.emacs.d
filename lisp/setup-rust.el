132f3672e3720b7bb0f6e2b51a5820650f32ebd5
Ricardo Martins <ricardo@scarybox.net>
2014-11-11 11:48:43 +0000

    Add whitespace rules and fix library paths for Rust.

--- lisp/setup-rust.el
+++ lisp/setup-rust.el
@@ -1,3 +1,18 @@
-(add-hook 'rust-mode-hook (lambda () (flycheck-mode 1)))
+(add-hook 'rust-mode-hook
+          (lambda ()
+            ;; Enable on-the-fly syntax checking
+            (flycheck-mode 1)
+            ;; Fix paths where to look for libraries
+            (add-to-list 'flycheck-rust-library-path
+                         (concat (projectile-project-root) "target"))
+            (add-to-list 'flycheck-rust-library-path
+                         (concat (projectile-project-root) "target/release"))
+            ;; Rust has different rules for too long lines
+            (setq-local fill-column 101)
+            (setq-local whitespace-line-column 100)
+            ;; Reload whitespace mode to make the previous change effective
+            (whitespace-mode -1)
+            (whitespace-mode 1)))
+
 
 (provide 'setup-rust)
