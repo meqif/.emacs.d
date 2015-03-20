8d3a3a25627b8e00da3102f662a04405f0c6483d
Ricardo Martins <ricardo@scarybox.net>
2015-03-20 17:09:54 +0000

    Use use-package for rust-mode.

--- init.el
+++ init.el
@@ -145,8 +145,29 @@
   'tex-mode      'setup-latex
   'markdown-mode 'setup-markdown
   'js2-mode      'setup-javascript
-  'rust-mode     'setup-rust
   'helm-bibtex   'setup-helm-bibtex)
+
+(use-package rust-mode
+  :config
+  (progn
+    (add-hook 'rust-mode-hook
+              (lambda ()
+                ;; Enable on-the-fly syntax checking
+                (flycheck-mode 1)
+                ;; Fix paths where to look for libraries
+                (add-to-list 'flycheck-rust-library-path
+                             (concat (projectile-project-root) "target"))
+                (add-to-list 'flycheck-rust-library-path
+                             (concat (projectile-project-root) "target/release"))
+                ;; Rust has different rules for too long lines
+                (setq-local fill-column 101)
+                (setq-local whitespace-line-column 100)
+                ;; Reload whitespace mode to make the previous change effective
+                (whitespace-mode -1)
+                (whitespace-mode 1)))
+
+    ;; Add brackets to smartparens pair list
+    (sp-local-pair 'rust-mode "<" ">")))
 
 (use-package flyspell
   :init
