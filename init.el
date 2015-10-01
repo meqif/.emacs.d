11762223ef849fa55db8a67f405e9f21e2a0e4a2
Ricardo Martins <ricardo@scarybox.net>
2015-10-01 15:52:32 +0100

    Easily move between use-package blocks.

--- init.el
+++ init.el
@@ -107,7 +107,15 @@
     (setq imenu-auto-rescan t)
 
     ;; Make imenu work on larger files
-    (setq imenu-auto-rescan-maxout 120000)))
+    (setq imenu-auto-rescan-maxout 120000)
+
+    ;; Easily move between use-package blocks
+    ;; https://gist.github.com/jordonbiondo/6385874a70420b05de18
+    (defun meqif/imenu-use-package ()
+      (add-to-list 'imenu-generic-expression
+                   '("Used Packages"
+                     "\\(^\\s-*(use-package +\\)\\(\\_<.+\\_>\\)" 2)))
+    (add-hook 'emacs-lisp-mode-hook #'meqif/imenu-use-package)))
 
 (use-package compile
   :config
