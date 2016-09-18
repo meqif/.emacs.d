a65d57f2b7fa9ea40fcaa7529199e64fe842feac
Ricardo Martins <ricardo@scarybox.net>
2016-09-18 13:41:35 +0100

    Don't use ivy-imenu-goto

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -49,7 +49,4 @@
      (interactive)
      (switch-to-buffer (get-buffer-create "*scratch*"))))
 
-;; ivy replacement for helm-imenu
-(global-set-key (kbd "s-r") #'ivy-imenu-goto)
-
 (provide 'keybindings)
