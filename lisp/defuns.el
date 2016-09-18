3dd1960852db1eab87667764357493a0de192900
Ricardo Martins <ricardo@scarybox.net>
2016-09-18 13:12:11 +0100

    Make fill-paragraph toggle behavior

--- lisp/defuns.el
+++ lisp/defuns.el
@@ -140,6 +140,18 @@ beginning of the line."
               (ivy-imenu-get-candidates-from (delete (assoc "*Rescan*" items) items))
               :action (lambda (k) (goto-char k)))))
 
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph] #'endless/fill-or-unfill)
+
 (defun gogo-fullscreen ()
   "Change to fullscreen mode.
 
