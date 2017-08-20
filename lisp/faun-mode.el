c0f7a056adb28b91f46e26623a27de1deb918fde
Ricardo Martins <ricardo@scarybox.net>
2017-08-20 13:39:43 +0100

    Add header and add custom variables types to faun-mode

--- lisp/faun-mode.el
+++ lisp/faun-mode.el
@@ -1,3 +1,25 @@
+;;; faun-mode.el --- Minor mode for automatic pandoc processing  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Ricardo Martins
+
+;; Author: Ricardo Martins
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24.3") (f "0.19.0"))
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;; http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;;; Code:
+
 (eval-when-compile (require 'f))
 
 (defgroup faun nil "Minor-mode for automatic pandoc processing."
@@ -5,10 +27,12 @@
 
 (defcustom faun-pandoc-output-format "markdown_github"
   "Desired pandoc output format."
+  :type 'string
   :group 'faun)
 
 (defcustom faun-pandoc-output-extensions '("footnotes" "yaml_metadata_block")
   "Syntax extensions for the output format."
+  :type '(repeat string)
   :group 'faun)
 
 (defun faun-mode--pandoc-after-save ()
@@ -45,3 +69,4 @@
   (if faun-mode (faun-mode-turn-on) (faun-mode-turn-off)))
 
 (provide 'faun-mode)
+;;; faun-mode.el ends here
