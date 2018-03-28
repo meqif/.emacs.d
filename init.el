196566bd8493559976b7d6c6060ac95171e58b9e
Ricardo Martins <ricardo@scarybox.net>
2018-03-28 22:56:56 +0100

    Add major mode and flycheck for Kotlin

--- init.el
+++ init.el
@@ -1239,6 +1239,13 @@ naming scheme."
   (add-hook 'ess-mode-hook 'company-mode)
   (add-hook 'inferior-ess-mode-hook 'company-mode))
 
+(use-package kotlin-mode)
+
+(use-package flycheck-kotlin
+  :after kotlin-mode
+  :config
+  (flycheck-kotlin-setup))
+
 ;; Improve readability of ELisp regular expressions
 (use-package easy-escape
   :init
