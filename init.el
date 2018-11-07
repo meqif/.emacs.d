abfa3e9fc4f808d343056ce1c15940bfd225b279
Ricardo Martins <ricardo@scarybox.net>
2018-11-07 20:59:51 +0000

    Increase the GC threshold to speed up the start process

--- init.el
+++ init.el
@@ -3,6 +3,10 @@
 (unless noninteractive
   (message "Loading %s..." load-file-name))
 
+;; Increase the threshold for garbage collection to speed up start process
+(setq gc-cons-threshold (* 256 1024 1024)
+      gc-cons-percentage 0.1)
+
 (setq user-init-file (or load-file-name buffer-file-name))
 (setq user-emacs-directory (file-name-directory user-init-file))
 
