ac8fe98bf9aabc40ff55a3c4264a962cd97e806e
Ricardo Martins <ricardo@scarybox.net>
2019-09-06 14:03:25 +0100

    Disable Eglot's auto-reconnect fix move to column

--- init.el
+++ init.el
@@ -608,7 +608,9 @@ INITIAL-DIRECTORY, if non-nil, is used as the root directory for search."
   :hook ((rust-mode kotlin-mode ruby-mode) . eglot-ensure)
   :config
   ;; Fix column calculation when ligatures are used
-  (setq eglot-current-column-function 'eglot-lsp-abiding-column)
+  (setq eglot-current-column-function 'eglot-lsp-abiding-column
+        eglot-move-to-column-function 'eglot-move-to-lsp-abiding-column
+        eglot-autoreconnect nil)
   (general-define-key :keymap 'eglot-mode-map "C-h ." 'eglot-help-at-point))
 
 (use-package flyspell
