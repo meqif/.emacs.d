dd5fff6ebb9f5f5729da18992dabbb78e3c4dfcb
Ricardo Martins <ricardo@scarybox.net>
2019-10-05 22:58:34 +0100

    Update eglot

--- init.el
+++ init.el
@@ -569,6 +569,7 @@ INITIAL-DIRECTORY, if non-nil, is used as the root directory for search."
   ;; Fix column calculation when ligatures are used
   (setq eglot-current-column-function 'eglot-lsp-abiding-column
         eglot-move-to-column-function 'eglot-move-to-lsp-abiding-column
+        eglot-autoshutdown t
         eglot-autoreconnect nil)
   (general-define-key :keymap 'eglot-mode-map "C-h ." 'eglot-help-at-point))
 
