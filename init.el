d42298caaac5ab88f3e9dd89930af62c3299c44e
Ricardo Martins <ricardo@scarybox.net>
2020-05-09 11:12:03 +0100

    Update eglot and remove unneccessary configuration overrides

--- init.el
+++ init.el
@@ -593,10 +593,7 @@ Serves as an alternative to projectile-find-file that doesn't depend on projecti
 (use-package eglot
   :hook ((rust-mode kotlin-mode ruby-mode js2-mode) . eglot-ensure)
   :config
-  ;; Fix column calculation when ligatures are used
-  (setq eglot-current-column-function 'eglot-lsp-abiding-column
-        eglot-move-to-column-function 'eglot-move-to-lsp-abiding-column
-        eglot-autoshutdown t
+  (setq eglot-autoshutdown t
         eglot-autoreconnect nil)
   (general-define-key :keymap 'eglot-mode-map "C-h ." 'eglot-help-at-point))
 
