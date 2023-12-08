4962caa53844c4bb15a673d0b508a8f4267a262d
Ricardo Martins <ricardo@scarybox.net>
2023-12-08 09:06:42 +0000

    Drop fussy, go back to plain orderless for completion-styles

--- init.el
+++ init.el
@@ -390,22 +390,7 @@
       `(orderless-literal . ,(s-chop-suffix "=" pattern))))
 
   (setq orderless-style-dispatchers '(without-if-bang exact-match-if-equals-suffix)
-        orderless-matching-styles '(orderless-initialism orderless-prefixes orderless-regexp)))
-
-;; Even greater control over completion
-(use-package fussy
-  :config
-  ;; Use orderless for filtering, flx-rs for fuzzy match scoring
-  (setq completion-styles '(fussy basic)
-        fussy-filter-fn 'fussy-filter-orderless))
-
-;; Vroom vroom
-(use-package flx-rs
-  :ensure t
-  :elpaca (flx-rs :repo "jcs-elpa/flx-rs" :host github :files (:defaults "bin"))
-  :config
-  (setq fussy-score-fn 'fussy-flx-rs-score)
-  (flx-rs-load-dyn))
+        orderless-matching-styles '(orderless-initialism orderless-prefixes)))
 
 (use-package embark
   :demand
