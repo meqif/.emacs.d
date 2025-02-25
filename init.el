a53f39116c1818e09ef0e8bdcb84dfa95047a692
Ricardo Martins <ricardo@scarybox.net>
2025-02-25 11:43:33 +0000

    Update elpaca version

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
       use-package-compute-statistics t)
 
 ;; Elpaca package manager!
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
